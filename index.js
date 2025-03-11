84dd1835d3b61d13399e9291c6e9c2808255fdd7
Calvinbullock <calvinnbullock@gmail.com>
2025-03-11 12:28:06 -0600

    moved another comment

--- index.js
+++ index.js
@@ -97,8 +97,3 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         dispayToPage("Could not get URL");
     }
 });
-
-
-// NOTE: for website time spent maybe have logic that only sites that are visited more then once
-//      or for longer then a user selected time are stored. That way your useage is not clutted
-//      by quick hops
