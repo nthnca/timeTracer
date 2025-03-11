142dfc2c51f5739bab282211fc32b43352d31748
Calvinbullock <calvinnbullock@gmail.com>
2025-03-11 14:29:59 -0600

    added cleanUrl function to background

--- background.js
+++ background.js
@@ -1,13 +1,35 @@
 
-import { extractBaseUrl } from './src/utils'
-
 // ===================================================== \\
 // ===================================================== \\
 //                      Utilities
 // ===================================================== \\
 // ===================================================== \\
 
+/* ===================================================== *\
+|| Extract the top level domain from the URL             ||
+\* ===================================================== */
+function cleanUrl(url) {
+    if (!url) {
+        return null;
+    }
 
+    try {
+        // remove all other gunk after target
+        const urlObj = new URL(url);
+        let baseUrl = urlObj.origin;
+
+        // remove `https://`
+        if (baseUrl.startsWith("https://")) {
+            return baseUrl.substring(8)
+        }
+
+        return baseUrl;
+
+    } catch (error) {
+        console.error("Invalid URL:", url, error);
+        return null;
+    }
+}
 
 // ===================================================== \\
 // ===================================================== \\
@@ -17,19 +39,19 @@ import { extractBaseUrl } from './src/utils'
 
 
 chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
-  if (changeInfo.url) {
-    console.log("URL changed: " + extractBaseUrl(changeInfo.url));
-  }
+    if (changeInfo.url) {
+        console.log("URL changed: " + cleanUrl(changeInfo.url));
+    }
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-  console.log(`Tab activated: Tab ID = ${activeInfo.tabId}`);
+    console.log(`Tab activated: Tab ID = ${activeInfo.tabId}`);
 
-  chrome.tabs.get(activeInfo.tabId, function(tab){
-    if(chrome.runtime.lastError){
-      console.error(chrome.runtime.lastError);
-      return;
-    }
-    console.log("Active Tab URL: ", extractBaseUrl(tab.url));
-  });
+    chrome.tabs.get(activeInfo.tabId, function(tab){
+        if(chrome.runtime.lastError){
+            console.error(chrome.runtime.lastError);
+            return;
+        }
+        console.log("Active Tab URL: ", cleanUrl(tab.url));
+    });
 });
