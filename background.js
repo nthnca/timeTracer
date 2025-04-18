3a9e8e773a115261145a3863459e27ca29a623da
Calvinbullock <calvinnbullock@gmail.com>
2025-04-18 10:44:03 -0700

    rewrote updateStoredData function, added more comments to main script

--- background.js
+++ background.js
@@ -13,7 +13,7 @@
 \* ===================================================== */
 function storeData(key, data) {
     chrome.storage.local.set({ [key]: data }).then(() => {
-        console.log("Value is set");
+        console.log(`Key: ${key}, Value: ${data}`);
     });
 }
 
@@ -22,21 +22,40 @@ function storeData(key, data) {
 \* ===================================================== */
 function retrieveData(key) {
     return chrome.storage.local.get([key]).then((result) => {
-        console.log("Value is " + result.key);
+        console.log(`Key: ${key}, Value: ${result.key}`);
     });
 }
 
 /* ===================================================== *\
 || Update Data points in local storage                   ||
 \* ===================================================== */
-function updateStoredData(url) {
+async function updateStoredData(activeUrl) {
+    var key = "siteList"
+    var siteList = await retrieveData(key);
 
+    // data storage struc
+    let newListItem = {
+        url: activeUrl, // string
+        lastAccses: "", // date TODO: this needs current date / time
+        totalTime: 0,   // in hours
+    }
+
+    // create list if null
+    if (!siteList) {
+        siteList = [];
+    }
+
+    // update list
+    siteList.push(newListItem);
+    console.log(siteList)           // DEBUG:
+    storeData(key, siteList);
 }
 
 
 /* ===================================================== *\
 || Extract the top level domain from the URL             ||
 \* ===================================================== */
+// TODO: maybe change this function name??
 function cleanUrl(url) {
     if (!url) {
         return null;
@@ -66,26 +85,29 @@ function cleanUrl(url) {
 // ===================================================== \\
 // ===================================================== \\
 
-let urlData = [
-    {
-        url: "google.com",
-        time: 60             // in minutes
-    },
-    {
-        url: "search.brave.com",
-        time: 60             // in minutes
-    },
-];
+// EXAMPLE DATA
+// let urlData = [
+//     {
+//         url: "google.com",
+//         time: 60             // in minutes
+//     },
+//     {
+//         url: "search.brave.com",
+//         time: 60             // in minutes
+//     },
+// ];
 
-let activeUrl = null;
-
+// checking if the current tab URL / site has changed
 chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
     if (changeInfo.url) {
-        activeUrl = cleanUrl(changeInfo.url);
+        // get url, then update siteList
+        let activeUrl = cleanUrl(changeInfo.url);
+        updateStoredData(activeUrl);
         console.log("URL changed: " + activeUrl); // DEBUG:
     }
 });
 
+// get current URL on tab change
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function(tab) {
         // handle tab errors
@@ -94,18 +116,10 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
             return;
         }
 
-        activeUrl = cleanUrl(changeInfo.url);
+        // get url, then update siteList
+        let activeUrl = cleanUrl(tab.url); // get new URL
+        updateStoredData(activeUrl);
         console.log("Active Tab URL: ", activeUrl); // DEBUG:
     });
 });
-
-let dataPoint = {
-    url: activeUrl, // string
-    lastAccses: "", // date TODO: this needs current date / time
-    totalTime: 0,   // in hours
-}
-
-updateStoredData(dataPoint);
-
-
 
