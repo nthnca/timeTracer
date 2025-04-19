8b257fdf154376b28749c3169f0a505f55f5b6ed
Calvinbullock <calvinnbullock@gmail.com>
2025-04-19 16:56:31 -0700

    updated layout for testing

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+#
 
 ## This is a chrome extension that will track a users time on individual websites and communicate that time back to the user.
 
@@ -5,10 +6,15 @@
 - local storage
 
 ## TODOs:
-- find a way to use imports form src/utils.js
-    - `background.js` sends error: imports are only allowed in module
-    - save to and from local storage are duplicated.... (background.js, popup/index.js)
-- add a way to flag URLs to remind people that they don't want to spend more time on that site
+- [ ] find a way to use imports form src/utils.js
+    - [ ] `background.js` sends error: imports are only allowed in module
+    - [ ] save to and from local storage are duplicated.... (background.js, popup/index.js)
+- [ ] add a way to flag URLs to remind people that they don't want to spend more time on that site
+- [ ] 
+
+## file layout
+- pkg/ [the final code to run the extension]
+- src/ [the tests and vanilla .js code, seperated from the chrome API code]
 
 ## Resources:
 - [Basics of Chrome Extensions](https://www.youtube.com/watch?v=Zt_6UXvoKHM)
--- pkg/background.js
+++ pkg/background.js
@@ -16,7 +16,7 @@ function storeData(key, data) {
         if (chrome.runtime.lastError) {
             console.error('Error saving to local storage:', chrome.runtime.lastError);
         } else {
-            console.log(`Stored - key: ${key}, value: ${JSON.stringify(result[key])}`);
+            console.log(`Stored - key: ${key}, value: ${JSON.stringify(data)}`);
         }
     });
 }
@@ -24,7 +24,7 @@ function storeData(key, data) {
 /* ===================================================== *\
 || Retrieve data from to local                           ||
 \* ===================================================== */
-async function retrieveData(key) {
+async function getData(key) {
     try {
         const result = await chrome.storage.local.get([key]);
         console.log(`retrieve - key: ${key}, value: ${result[key]}`);
@@ -33,31 +33,6 @@ async function retrieveData(key) {
         console.error("Error retrieving data:", error);
         return undefined;
     }
-}
-
-/* ===================================================== *\
-|| Update Data points in local storage                   ||
-\* ===================================================== */
-async function updateStoredData(activeUrl) {
-    var key = "siteList"
-    var siteList = await retrieveData(key);
-
-    // data storage struc
-    let newListItem = {
-        url: activeUrl, // string
-        lastAccses: "", // date TODO: this needs current date / time
-        totalTime: 0,   // in hours
-    }
-
-    // create list if null
-    if (!siteList) {
-        siteList = [];
-    }
-
-    // update list
-    siteList.push(newListItem);
-    console.log(siteList)           // DEBUG:
-    storeData(key, siteList);
 }
 
 /* ===================================================== *\
@@ -89,7 +64,7 @@ function cleanUrl(url) {
 
         // remove `https://`
         if (baseUrl.startsWith("https://")) {
-            return baseUrl.substring(8)
+            return baseUrl.substring(8);
         }
 
         return baseUrl;
@@ -100,23 +75,52 @@ function cleanUrl(url) {
     }
 }
 
-// ===================================================== \\
-// ===================================================== \\
-//                      Main Script
-// ===================================================== \\
-// ===================================================== \\
+/* ===================================================== *\
+|| Update Data points in local storage                   ||
+\* ===================================================== */
+async function updateStoredData(activeUrl) {
+    let key = "siteList"
+    let siteList = await getData(key);
 
-// EXAMPLE DATA
-// let urlData = [
-//     {
-//         url: "google.com",
-//         time: 60             // in minutes
-//     },
-//     {
-//         url: "search.brave.com",
-//         time: 60             // in minutes
-//     },
-// ];
+    // create list if null
+    if (!siteList) {
+        siteList = [];
+    }
+
+    // find urls index in list
+    let urlIndex = searchDataUrls(activeUrl, siteList);
+
+    // if not in list
+    if (urlIndex == -1) {
+
+        // data storage struc
+        let newListItem = {
+            url: activeUrl, // string
+            enterDate: "", // date TODO: this needs current date / time
+            totalTime: 0,   // in hours
+        }
+
+        // update list
+        siteList.push(newListItem);
+        console.log(siteList);           // DEBUG:
+        storeData(key, siteList);
+    } else { // if in list
+        let item = siteList[urlIndex];
+        // TODO: handle time increment and other bits
+
+        // calc usage time
+        let currDate = new Date();
+        let itemDate = item.enterDate;
+        let timeDiff = currDate.getTime() - itemDate.getTime();
+        console.log(timeDiff);
+    }
+}
+
+// ===================================================== \\
+// ===================================================== \\
+//              Chromium API Event Listeners             \\
+// ===================================================== \\
+// ===================================================== \\
 
 // checking if the current tab URL / site has changed
 chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
@@ -143,4 +147,18 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
         console.log("Active Tab URL: ", activeUrl); // DEBUG:
     });
 });
+
+// chrome window leave, enter
+chrome.windows.onFocusChanged.addListener(function(windowId) {
+    if (windowId === chrome.windows.WINDOW_ID_NONE) {
+        console.log("All Chrome windows are now unfocused.");
+        // Your logic for when Chrome is unfocused
+
+    } else {
+        console.log(`Chrome window with ID ${windowId} is now focused.`);
+        // Your logic for when a Chrome window is focused
+    }
+});
+
+
 
