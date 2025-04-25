5937ac92ad040768235df0495e83cd77be6ce655
Calvinbullock <calvinnbullock@gmail.com>
2025-04-25 15:27:43 -0700

    build script updated

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 
-$destinationFile =  "pkg/test_background.js"
+set -e
+
+destinationFile="pkg/test_background.js"
+
+touch "$destinationFile"
 
 # grab all the code between BEGIN_IMPORT_HERE & END_IMPORT_HERE from src/utils and dump to src/utils.js
-sed -n '/BEGIN_IMPORT_HERE/,/END_IMPORT_HERE/p' src/test_utils.js | sed '1d;$d' > $destinationFile
+sed -n '/BEGIN_IMPORT_HERE/,/END_IMPORT_HERE/p' src/test_utils.js | sed '1d;$d' > "$destinationFile"
 
-cat "src/test_utils.js" >> $destinationFile
+cat "src/chrome_api.js" >>  "$destinationFile"
 
 echo "build Complete at ($destinationFile)"
