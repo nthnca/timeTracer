6f78ba65078e11eb1959040f607517668c84bdc1
Calvinbullock <calvinnbullock@gmail.com>
2025-05-05 22:03:15 -0700

    added urlDataObj.js to front_end build

--- build.sh
+++ build.sh
@@ -1,19 +1,25 @@
 
 set -e
 
+destinationFile_BE="pkg/background.js"
+destinationFile_FE="pkg/popup/index.js"
+
 #
 ## front_end
 #
 
-destinationFile_BE="pkg/background.js"
-destinationFile_FE="pkg/popup/index.js"
-
 rm "$destinationFile_FE"
 touch "$destinationFile_FE"
 
+# urlDataObj
+# grab all the code between BEGIN_IMPORT_HERE & END_IMPORT_HERE from src/urlDataObj and dump to pkg/background.js
+sed -n '/BEGIN_IMPORT_HERE/,/END_IMPORT_HERE/p' src/urlDataObj.js | sed '1d;$d' >> "$destinationFile_FE"
+
+# chrome_api
 # grab all the code between BEGIN_IMPORT_HERE & END_IMPORT_HERE from src/urlDataObj and dump to pkg/background.js
 sed -n '/ADD_TO_FRONT_END_START/,/ADD_TO_FRONT_END_END/p' src/chrome_api.js | sed '1d;$d' >> "$destinationFile_FE"
 
+# front_end
 # grab all the code between BEGIN_IMPORT_HERE & END_IMPORT_HERE from src/urlDataObj and dump to pkg/background.js
 sed -n '/BEGIN_IMPORT_HERE/,/END_IMPORT_HERE/p' src/front_end.js | sed '1d;$d' >> "$destinationFile_FE"
 
@@ -26,12 +32,15 @@ echo "build Complete at ($destinationFile_FE)"
 rm "$destinationFile_BE"
 touch "$destinationFile_BE"
 
+# urlDataObj
 # grab all the code between BEGIN_IMPORT_HERE & END_IMPORT_HERE from src/urlDataObj and dump to pkg/background.js
 sed -n '/BEGIN_IMPORT_HERE/,/END_IMPORT_HERE/p' src/urlDataObj.js | sed '1d;$d' >> "$destinationFile_BE"
 
+# utils
 # grab all the code between BEGIN_IMPORT_HERE & END_IMPORT_HERE from src/utils and dump to pkg/background.js
 sed -n '/BEGIN_IMPORT_HERE/,/END_IMPORT_HERE/p' src/utils.js | sed '1d;$d' >> "$destinationFile_BE"
 
+# chrome_api
 cat "src/chrome_api.js" >>  "$destinationFile_BE"
 
 # build the front end first so this can run after the ft-end is done
--- pkg/popup/index.js
+++ pkg/popup/index.js
@@ -1,28 +1,277 @@
 
 // ===================================================== \\
 // ===================================================== \\
+//                     UrlDataObj
+// ===================================================== \\
+// ===================================================== \\
+
+class UrlDataObj {
+    constructor() {
+        this.activeUrl = null;
+        this.startTime = null
+        this.urlList = [];
+    }
+
+    /**
+    * Retrieves the currently active URL being tracked.
+    *
+    * @returns {string} - The currently active URL, or an empty string if no URL is active.
+    */
+    getActiveUrl() {
+        return this.activeUrl;
+    }
+
+    /**
+    * Sets the currently active URL being tracked and updates the tracking start time.
+    *
+    * @param {string} url - The URL to set as the currently active one.
+    * @param {Date} [currentTime=new Date()] - An optional Date object representing the
+    * starting time for this active URL. Defaults to the current timestamp if not provided,
+    * allowing for easier testing.
+    */
+    appendListItem(url) {
+        if (!this.urlList.some(item => item.url === url)) {
+            this.urlList.push( { url: url, totalTime: 0 } );
+            return true; // Indicate that a new item was appended
+        }
+        return false; // Indicate that the item already existed
+    }
+
+    /**
+    * Starts a new tracking session for a given URL.
+    * It sets the 'activeUrl' to the provided URL and the 'startTime' to the
+    * provided 'currentTime' (or the current timestamp if not provided).
+    * Logs an error and returns false if 'startTime' or 'activeUrl' are already
+    * truthy when attempting to start a new session.
+    *
+    * @param {string} url - The URL to set as the currently active one to start tracking.
+    * @param {Date} [currentTime=new Date()] - An optional Date object representing the
+    * starting time of the session. Defaults to the current timestamp if not provided,
+    * allowing for easier testing.
+    * @returns {boolean} - True if the session was started successfully, false otherwise
+    * (e.g., if a session was already active).
+    */
+    startSession(url, currentTime = new Date()) {
+        if (this.startTime || this.activeUrl) {
+            // TODO: write a test for this path
+            console.error("Error: startTime / activeUrl should never be true on enter ",
+                "old startTime: ", this.startTime,
+                "old activeUrl: ", this.activeUrl,
+                "new startTime: ", currentTime,
+                "new activeUrl: ", url
+            );
+            return false;
+        }
+
+        this.activeUrl = url;
+        this.startTime = currentTime;
+    }
+
+    /**
+    * Ends the currently active tracking session and records the elapsed time.
+    * It finds the active URL in the urlList, calculates the time elapsed since
+    * the 'startTime', adds it to the 'totalTime' of the corresponding item,
+    * and resets the 'startDate' and 'isActive' properties of that item.
+    * It also resets the 'activeUrl' and 'startTime' of the TrackingData object.
+    * Logs an error if no active item is found.
+    *
+    * @param {Date} [currentTime=new Date()] - An optional Date object representing the
+    * ending time of the session. Defaults to the current timestamp if not provided,
+    * allowing for easier testing.
+    */
+    endSession(currentTime = new Date()) {
+        if (this.activeUrl == null) {
+            console.error("Error: activeItem was null when endSession was called.");
+            return false;
+        }
+
+        const activeItem = this.urlList.find(item => item.url === this.activeUrl);
+        const elapsedTime = this.calcTimeElapsed(this.startTime, currentTime);
+
+        if (activeItem) {
+            activeItem.totalTime += elapsedTime;
+
+        } else {
+            // TODO: update tests to cover this case
+            // add item to list
+            this.urlList.push( {
+                url: this.activeUrl,
+                totalTime: elapsedTime
+            })
+        }
+        this.activeUrl = null;
+        this.startTime = null;
+    }
+
+    /**
+     * Converts the TrackingData object into a JSON string.
+     * Date objects are converted to ISO 8601 string format for serialization.
+     *
+     * @returns {string} - A JSON string representing the TrackingData.
+     * The string includes 'activeUrl', 'startTime' (as an ISO string or null), and
+     * 'urlList' (an array of objects with 'url' and 'totalTime' properties).
+     */
+    toJSONString() {
+        const jsonObject = {
+            activeUrl: this.activeUrl,
+            startTime: this.startTime ? this.startTime.toISOString() : null,
+            urlList: this.urlList.map(item => ({
+                url: item.url,
+                totalTime: item.totalTime
+            }))
+        };
+        return JSON.stringify(jsonObject);
+    }
+
+    /**
+    * Creates a new UrlDataObj instance from a JSON string.
+    * It attempts to parse the JSON string and populate the properties
+    * of a new UrlDataObj. Date strings in the JSON are converted back
+    * to Date objects. Handles potential JSON parsing errors and returns null
+    * in case of an error.
+    *
+    * @param {string} jsonString - The JSON string to parse.
+    * @returns {UrlDataObj|null} - A new UrlDataObj instance populated with
+    * data from the JSON string, or null if an error occurred during parsing.
+    */
+    fromJSONString(jsonString) {
+        try {
+            // check the obj is of the right type
+            if (!(typeof jsonString === "string")) {
+                console.error("Error: jsonString not instance of String - fromJSONString()");
+                console.error("jsonString Typeof:", typeof jsonString);
+            }
+            const jsonObj = JSON.parse(jsonString);
+
+            const trackingData = new UrlDataObj();
+            trackingData.activeUrl = jsonObj.activeUrl;
+            trackingData.startTime = jsonObj.startTime ? new Date(jsonObj.startTime) : null;
+            trackingData.urlList = jsonObj.urlList ? jsonObj.urlList.map(item => ({
+                url: item.url,
+                totalTime: item.totalTime
+            })) : [];
+
+            return trackingData;
+
+        } catch (error) {
+            console.error("Error parsing JSON:", error);
+            return null;
+        }
+    }
+
+    /**
+    * Calculates the time elapsed between a given start date and the current time, in milliseconds.
+    *
+    * @param {Date} useStartDate - The starting date to calculate the elapsed time from.
+    * @returns {number} The time elapsed in milliseconds.
+    */
+    calcTimeElapsed(startDate, endDate) {
+
+        // TODO: the line below this might be better then the current one
+        //if (Object.prototype.toString.call(startDate) !== '[object Date]' || isNaN(startDate)) {
+        if (Object.prototype.toString.call(startDate) !== '[object Date]') {
+            console.error("TypeError: Parameter 'startDate' in calcTimeElapsed() must be a Date object.", startDate);
+            console.trace();
+            return null; // Or throw error
+        }
+
+        return endDate - startDate;
+    }
+}
+
+
+// ==================================================== \\
+// ==================================================== \\
+// functions dependent on chrome API                    \\
+// ==================================================== \\
+// ==================================================== \\
+
+/**
+ * Stores data in Chrome's local storage.
+ *
+ * This function saves the provided data under the specified key in Chrome's local storage.
+ * It handles potential errors during the storage process and logs the outcome.
+ *
+ * @param {string} key - The key under which to store the data.
+ * @param {any} data - The data to be stored.  This can be any JavaScript object that is serializable.
+ */
+async function storeChromeLocalData(key, data) {
+    chrome.storage.local.set({ [key]: data}, function() {
+        if (chrome.runtime.lastError) {
+            console.error('Error saving to local storage:', chrome.runtime.lastError);
+        } else {
+            console.log(`Stored - key: ${key}, value: ${data}`);
+        }
+    });
+}
+
+/**
+ * Retrieves data from Chrome's local storage.
+ *
+ * This asynchronous function retrieves data from Chrome's local storage using the provided key.
+ * It handles potential errors during retrieval and returns the data or undefined if an error occurs.
+ *
+ * @param {string} key - The key of the data to retrieve.
+ * @returns {Promise<any | undefined>} A Promise that resolves with the retrieved data, or undefined if an error occurred.
+ */
+async function getChromeLocalData(key) {
+    try {
+        const result = await chrome.storage.local.get([key]);
+        console.log(`retrieve - key: ${key}, value: ${result[key]}`);
+        return result[key];
+
+    } catch (error) {
+        console.error("Error retrieving data:", error);
+        return undefined;
+    }
+}
+
+/**
+ * Retrieves site data from Chrome local storage.  If no data exists,
+ * it creates a new tracking object.
+ *
+ * @async
+ * @function getSiteObjData
+ * @returns {Promise<any>} A Promise that resolves with the site data object.
+ */
+async function getSiteObjData() {
+    let key = "siteData";
+    let siteDataString = await getChromeLocalData(key);
+
+    let siteDataObj = new UrlDataObj();
+
+    // if the data exists parse it into siteDataObj
+    if (siteDataString) {
+        siteDataObj = siteDataObj.fromJSONString(siteDataString);
+    }
+
+    // check the obj is of the right type
+    if (!(siteDataObj instanceof UrlDataObj)) {
+        console.error( "Error: siteData is not instance of UrlDataObj - in getSiteObjData()",);
+    }
+    return siteDataObj;
+}
+
+/**
+ * Stores site data to Chrome local storage.
+ *
+ * @async
+ * @function setSiteObjData
+ * @param {any} siteDataObj - The site data object to store.
+ * @returns {Promise<void>} A Promise that resolves when the data is successfully stored.
+ */
+async function setSiteObjData(siteDataObj) {
+    let key = "siteData";
+    const siteDataString = siteDataObj.toJSONString();
+    storeChromeLocalData(key, siteDataString);
+}
+
+
+// ===================================================== \\
+// ===================================================== \\
 //                      Utilities
 // ===================================================== \\
 // ===================================================== \\
-
-
-/* ===================================================== *\
-|| Store data to local                                   ||
-\* ===================================================== */
-function storeData(key, data) {
-    chrome.storage.local.set({ [key]: data }).then(() => {
-        console.log("Value is set");
-    });
-}
-
-/* ===================================================== *\
-|| Retrieve data from to local                           ||
-\* ===================================================== */
-function retrieveData(key) {
-    return chrome.storage.local.get([key]).then((result) => {
-        console.log("Value is " + result.key);
-    });
-}
 
 /* ===================================================== *\
 || Format and display the collected data for each site   ||
@@ -55,51 +304,4 @@ function dispayToPage(currentUrl) {
 // ===================================================== \\
 // ===================================================== \\
 
-// TODO: update like the demo below
-let urlData = ["Google.com - 2 hours", "search.Brave.com - 1 hour"];
 
-// NOTE: Demo of data
-//let urlData = [
-//    {
-//        url: "google.com",
-//        time: 60             // in minutes
-//    },
-//    {
-//        url: "search.brave.com",
-//        time: 60             // in minutes
-//    },
-//];
-
-// Get current active tab URL
-//      Generated by: Google Gemini (Bard)
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    let currentUrl = null;
-
-    // check if tabs exist
-    if (tabs && tabs.length > 0) {
-        const currentTab = tabs[0];
-        currentUrl = currentTab.url;
-
-    } else {
-        document.getElementById('currUrl').textContent = "Could not find active tab";
-    }
-
-    // if we have current url add to local store
-    if (currentUrl) {
-        // store new url
-        retrieveData("urlData").then(storedData => {
-            if(storedData){
-                urlData = storedData;
-            }
-
-            // update stored data
-            urlData.push(currentUrl);
-            storeData("urlData", urlData);
-        })
-
-        dispayToPage(currentUrl);
-
-    } else {
-        dispayToPage("Could not get URL");
-    }
-});
